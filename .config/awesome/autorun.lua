05ed294886c178286c6097388ae844075ecd0e77
Niek Bruins <niek@fambruins.nl>
2015-03-16 13:41:04 +0100

    awesome config

---
+++ .config/awesome/autorun.lua
@@ -0,0 +1,21 @@
+local awful = require("awful")
+
+local autorun = {}
+
+function autorun.run_once(cmd)
+  findme = cmd
+  firstspace = cmd:find(" ")
+  if firstspace then
+     findme = cmd:sub(0, firstspace-1)
+  end
+  awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
+end
+
+function autorun.run()
+	autorun.run_once("urxvtd")
+	autorun.run_once("unclutter")
+	autorun.run_once("compton -GCb")
+end
+
+return autorun
+
---
+++ .config/awesome/dualscreen.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Usage: "dual-screen" sets your laptop screen (LVDS1) as primary screen and the other display (HDMI1 or VGA1) on its right, "dual-screen l" is the same but left sided.
+
+case $1 in
+"l")
+  position="--left-of";;
+*)
+  position="--right-of";;
+esac
+if [ `xrandr | grep -c ' connected '` -eq 2 ]; then # dual-monitor
+    echo -n "Dual screen: "
+    if [ `xrandr | grep VGA1 | grep -c ' connected '` -eq 1 ]; then
+        echo "VGA1 +  LVDS1"
+        xrandr --output VGA1 --auto --output LVDS1 --auto --primary $position VGA1
+    fi
+    if [ `xrandr | grep HDMI1 | grep -c ' connected '` -eq 1 ]; then
+        echo "HDMI1 + LVDS1"
+        xrandr --output HDMI1 --auto --output LVDS1 --auto --primary $position HDMI1
+    fi
+else
+    echo "Single screen: LVDS1"
+xrandr --output LVDS1 --auto --primary --output VGA1 --off --output HDMI1 --off
+fi
+
