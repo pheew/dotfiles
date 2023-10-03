c7aff7c6de1ecb75621fbd9e2f07277d9d15dc87
Niek Bruins <niek@fambruins.nl>
2023-10-03 11:36:28 +0200

    feat(vim): use conform.nvim for formatting

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -1,14 +1,46 @@
 return {
     -- Adds code formatting support
     {
-        "sbdchd/neoformat",
+        "stevearc/conform.nvim",
+        event = { "BufWritePre" },
+        dependencies = { "mason.nvim" },
+        lazy = true,
+        cmd = "ConformInfo",
         keys = {
             {
                 "<leader>f",
-                "<cmd>Neoformat<CR>",
+                function()
+                    require("conform").format({ async = true, lsp_fallback = true })
+                end,
                 desc = "Format file",
                 noremap = true,
                 silent = true
+            }
+        },
+        opts = {
+            format_on_save = {
+                lsp_fallback = true,
+                timeout_ms = 500
+            },
+            formatters_by_opts = {
+                shell = { { "shfmt" } },
+                lua = { "stylua" },
+                javascript = { { "prettierd", "prettier" } },
+                javascriptreact = { { "prettierd", "prettier" } },
+                typescript = { { "prettierd", "prettier" } },
+                typescriptreact = { { "prettierd", "prettier" } },
+                vue = { { "prettierd", "prettier" } },
+                css = { { "prettierd", "prettier" } },
+                scss = { { "prettierd", "prettier" } },
+                less = { { "prettierd", "prettier" } },
+                html = { { "prettierd", "prettier" } },
+                json = { { "prettierd", "prettier" } },
+                jsonc = { { "prettierd", "prettier" } },
+                yaml = { { "prettierd", "prettier" } },
+                markdown = { { "prettierd", "prettier" } },
+                ["markdown.mdx"] = { { "prettierd", "prettier" } },
+                graphql = { { "prettierd", "prettier" } },
+                handlebars = { { "prettierd", "prettier" } }
             }
         }
     }
