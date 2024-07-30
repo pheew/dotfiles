2a67b2ffd652562e29c5b3dc12f6b4dd6a0ddafc
Niek Bruins <niek@fambruins.nl>
2024-07-30 10:43:47 +0200

    fix(vim): improve formatter config

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -1,44 +1,49 @@
 return {
-    -- Adds code formatting support
-    {
-        "stevearc/conform.nvim",
-        event = { "BufWritePre" },
-        dependencies = { "mason.nvim" },
-        lazy = true,
-        cmd = "ConformInfo",
-        keys = {
-            {
-                "<leader>f",
-                function()
-                    require("conform").format({ async = true, lsp_fallback = true })
-                end,
-                desc = "Format file",
-                noremap = true,
-                silent = true
-            }
-        },
-        opts = {
-            formatters_by_ft = {
-                shell = { { "shfmt" } },
-                sh = { { "shfmt" } },
-                lua = { "stylua" },
-                javascript = { { "prettierd", "prettier" } },
-                javascriptreact = { { "prettierd", "prettier" } },
-                typescript = { { "prettierd", "prettier" } },
-                typescriptreact = { { "prettierd", "prettier" } },
-                vue = { { "prettierd", "prettier" } },
-                css = { { "prettierd", "prettier" } },
-                scss = { { "prettierd", "prettier" } },
-                less = { { "prettierd", "prettier" } },
-                html = { { "prettierd", "prettier" } },
-                json = { { "prettierd", "prettier" } },
-                jsonc = { { "prettierd", "prettier" } },
-                yaml = { { "prettierd", "prettier" } },
-                markdown = { { "prettierd", "prettier" } },
-                ["markdown.mdx"] = { { "prettierd", "prettier" } },
-                graphql = { { "prettierd", "prettier" } },
-                handlebars = { { "prettierd", "prettier" } }
-            }
-        }
-    }
+	-- Adds code formatting support
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		dependencies = { "mason.nvim" },
+		lazy = true,
+		cmd = "ConformInfo",
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				desc = "Format file",
+				noremap = true,
+				silent = true,
+			},
+		},
+		opts = {
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			formatters_by_ft = {
+				shell = { { "shfmt" } },
+				sh = { { "shfmt" } },
+				lua = { "stylua" },
+				javascript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
+				vue = { { "prettierd", "prettier" } },
+				css = { { "prettierd", "prettier" } },
+				scss = { { "prettierd", "prettier" } },
+				less = { { "prettierd", "prettier" } },
+				html = { { "prettierd", "prettier" } },
+				json = { { "prettierd", "prettier" } },
+				jsonc = { { "prettierd", "prettier" } },
+				yaml = { { "prettierd", "prettier" } },
+				markdown = { { "prettierd", "prettier" } },
+				["markdown.mdx"] = { { "prettierd", "prettier" } },
+				graphql = { { "prettierd", "prettier" } },
+				handlebars = { { "prettierd", "prettier" } },
+			},
+		},
+	},
 }
