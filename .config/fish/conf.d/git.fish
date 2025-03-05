31e4f16eca68aa3583d2ae17d890a01c498d6926
Niek Bruins <niek@fambruins.nl>
2025-03-05 15:33:12 +0100

    feat(fish): add fish shell

---
+++ .config/fish/conf.d/git.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+
+    abbr --add gco git checkout
+
+    abbr --add gst git status
+    abbr --add gss git status --short
+
+    abbr --add gpr git pull --rebase
+
+    abbr --add ga git add
+    abbr --add gapa git add --patch
+
+    abbr --add gcmsg git commit -m
+
+end
