11bb34de517e4f85a7baefdcc330e57abead791f
Niek Bruins <niek@fambruins.nl>
2025-03-25 12:55:03 +0100

    feat(fish): add some more abbr

--- .config/fish/conf.d/git.fish
+++ .config/fish/conf.d/git.fish
@@ -1,15 +1,31 @@
 if status is-interactive
+    abbr --add g git
 
+    #checkout
     abbr --add gco git checkout
 
+    # fetch
+    abbr --add gf git fetch
+
+    # status
     abbr --add gst git status
     abbr --add gss git status --short
 
+    # pull
     abbr --add gpr git pull --rebase
 
+    # add
     abbr --add ga git add
     abbr --add gapa git add --patch
 
+    # commit
+    abbr --add gc! git commit --amend
     abbr --add gcmsg git commit -m
 
+    # diff
+    abbr --add gd git diff
+    abbr --add gds git diff --staged
+
+    # log
+    abbr --add glol git log
 end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,20 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+   
+    # Enable vi mode
+    fish_vi_key_bindings
+
+
+    # force commands to output colored output
+    set -gx CLICOLOR_FORCE 1
+  
+    # pipe shortcuts
+    abbr --add --position anywhere G '| grep'
+    abbr --add --position anywhere H '| head'
+    abbr --add --position anywhere T '| tail'
+
+    abbr --add reload fish
+
+    abbr --add l ls -lFh
 
 end
