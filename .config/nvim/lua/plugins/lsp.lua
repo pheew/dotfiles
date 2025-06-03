bee0d2a2dbc000b533abdddb1d867f76444af3b3
Niek Bruins <niek@fambruins.nl>
2025-06-03 14:44:00 +0200

    feat(vim): update plugins and fix lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -76,7 +76,7 @@ return {
 				jsonls = {},
 				bashls = {},
 				dockerls = {},
-				tsserver = {},
+				ts_ls = {},
 				html = { filetypes = { "html", "twig", "hbs" } },
 				-- react = {},
 				cssls = {},
@@ -98,67 +98,79 @@ return {
 
 			mason_lspconfig.setup({
 				ensure_installed = vim.tbl_keys(servers),
+				automatic_enable = true,
 			})
 
-			local on_attach = function(_, bufnr)
-				vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					-- Unset 'formatexpr'
+					vim.bo[args.buf].formatexpr = nil
+					-- Unset 'omnifunc'
+					vim.bo[args.buf].omnifunc = nil
+					-- Unmap K
+					vim.keymap.del("n", "K", { buffer = args.buf })
 
-				vim.keymap.set("n", "gD", "<cmd>FzfLua lsp_declarations<CR>", { buffer = bufnr, desc = "Go to Declaration" })
-				vim.keymap.set(
-					"n",
-					"gd",
-					"<cmd>FzfLua lsp_definitions jump_to_single_result=true<CR>",
-					{ buffer = bufnr, desc = "Go to Definition" }
-				)
-				vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { buffer = bufnr })
-				vim.keymap.set(
-					"n",
-					"gi",
-					"<cmd>FzfLua lsp_implementations<CR>",
-					{ buffer = bufnr, desc = "Go to Implementation" }
-				)
-				vim.keymap.set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { buffer = bufnr })
-				vim.keymap.set(
-					"n",
-					"<leader>D",
-					"<cmd>FzfLua lsp_typedefs<CR>",
-					{ buffer = bufnr, desc = "Show type definition" }
-				)
-				vim.keymap.set(
-					"n",
-					"<leader>rn",
-					"<cmd>lua vim.lsp.buf.rename()<CR>",
-					{ buffer = bufnr, desc = "Rename symbol" }
-				)
-				vim.keymap.set("n", "gr", ":FzfLua lsp_references<CR>", { buffer = bufnr, desc = "Go to references" })
-				vim.keymap.set(
-					{ "n", "v" },
-					"<leader>ra",
-					"<cmd>lua vim.lsp.buf.code_action()<CR>",
-					{ buffer = bufnr, desc = "Code Actions" }
-				)
-				-- Disabled because I'm using trouble.nvim for this
-				-- vim.keymap.set("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", { buffer = bufnr })
-				-- vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { buffer = bufnr })
-				-- vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", { buffer = bufnr })
-				vim.keymap.set(
-					"n",
-					"<leader>of",
-					"<cmd>FzfLua lsp_document_symbols<CR>",
-					{ buffer = bufnr, desc = "Jump to document symbol" }
-				)
-			end
+					local bufnr = args.buf
 
-			mason_lspconfig.setup_handlers({
-				function(server_name)
-					require("lspconfig")[server_name].setup({
-						capabilities = capabilities,
-						on_attach = on_attach,
-						settings = servers[server_name],
-						filetypes = (servers[server_name] or {}).filetypes,
-					})
+					vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
+
+					vim.keymap.set("n", "gD", "<cmd>FzfLua lsp_declarations<CR>", { buffer = bufnr, desc = "Go to Declaration" })
+					vim.keymap.set(
+						"n",
+						"gd",
+						"<cmd>FzfLua lsp_definitions jump_to_single_result=true<CR>",
+						{ buffer = bufnr, desc = "Go to Definition" }
+					)
+					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { buffer = bufnr })
+					vim.keymap.set(
+						"n",
+						"gi",
+						"<cmd>FzfLua lsp_implementations<CR>",
+						{ buffer = bufnr, desc = "Go to Implementation" }
+					)
+					vim.keymap.set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { buffer = bufnr })
+					vim.keymap.set(
+						"n",
+						"<leader>D",
+						"<cmd>FzfLua lsp_typedefs<CR>",
+						{ buffer = bufnr, desc = "Show type definition" }
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>rn",
+						"<cmd>lua vim.lsp.buf.rename()<CR>",
+						{ buffer = bufnr, desc = "Rename symbol" }
+					)
+					vim.keymap.set("n", "gr", ":FzfLua lsp_references<CR>", { buffer = bufnr, desc = "Go to references" })
+					vim.keymap.set(
+						{ "n", "v" },
+						"<leader>ra",
+						"<cmd>lua vim.lsp.buf.code_action()<CR>",
+						{ buffer = bufnr, desc = "Code Actions" }
+					)
+					-- Disabled because I'm using trouble.nvim for this
+					-- vim.keymap.set("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", { buffer = bufnr })
+					-- vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { buffer = bufnr })
+					-- vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", { buffer = bufnr })
+					vim.keymap.set(
+						"n",
+						"<leader>of",
+						"<cmd>FzfLua lsp_document_symbols<CR>",
+						{ buffer = bufnr, desc = "Jump to document symbol" }
+					)
 				end,
 			})
+
+			-- mason_lspconfig.setup_handlers({
+			-- 	function(server_name)
+			-- 		require("lspconfig")[server_name].setup({
+			-- 			capabilities = capabilities,
+			-- 			on_attach = on_attach,
+			-- 			settings = servers[server_name],
+			-- 			filetypes = (servers[server_name] or {}).filetypes,
+			-- 		})
+			-- 	end,
+			-- })
 		end,
 	},
 }
